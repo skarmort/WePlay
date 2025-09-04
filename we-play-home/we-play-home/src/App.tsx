3b3df882362a0be58c3c74c0ae838f04d4621d1b
skarmort <oscarekoosa@gmail.com>
2025-09-04 14:40:32 +0200

    modified:   we-play-home/we-play-home/src/App.css 	modified:   we-play-home/we-play-home/src/App.tsx 	new file:   we-play-home/we-play-home/src/weplaylogo.svg

--- we-play-home/we-play-home/src/App.tsx
+++ we-play-home/we-play-home/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './weplaylogo.svg';
 import './App.css';
 
 function App() {
@@ -7,17 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <p className="weplay-cartoon-1" > 
+         WePlay!</p> 
+         
+         <p className="weplay-cartoon-1"> We Came. We Saw. We Dreamt. We Played!<br />
+         For the players! </p>
+
       </header>
     </div>
   );
